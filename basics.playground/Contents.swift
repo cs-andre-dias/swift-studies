9b652778597c167bd04b4b99f54a3583f656399f
André Dias <andre.dias@concrete.com.br>
2016-09-08 18:10:28 -0300

    Adiciona arquivo optionals e altera nome do arquivo variables
    Retira a parte sobre optionals do arquivo variables e adiciona no arquivo optionals, e altera o nome do arquivo variables para basics

--- basics.playground/Contents.swift
+++ basics.playground/Contents.swift
@@ -105,39 +105,4 @@ var range = primeiraPosicao...end
 
 hello[range]
 
-// Optionals
-
-var optionalTest: String?
-
-optionalTest = "404"
-
-// print(optionalTest)
-
-//if optionalTest != nil {
-//    let theOptional = optionalTest!
-//    print(theOptional)
-//}
-
-// Uma melhor opcao é usar o unwrapping atraves do if com variavel temporaria
-
-//if let theOptional = optionalTest{
-//    print(theOptional)
-//}
-
-// pode-se juntar mais de uma condicao no teste do if let
-
-//por exemplo: 
-
-if let theOptional = optionalTest, optionalInt = Int(theOptional)
-    where optionalInt == 404{
-    print(theOptional)
-}else{
-    print("Erro")
-}
-
-// Aqui em cima ele esta verificando se tem algum valor dentro da variavel
-// se tem ele verifica se pode transforma em integer com o metodo Int()
-// e ainda verifica se o resultado dessa conversao e o inteiro 404 com o where
-
-
 
