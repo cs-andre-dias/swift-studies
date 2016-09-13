dede52fdcce68609697059b139e1c172da5e306a
Andre Dias <andre.dias@concrete.com.br>
2016-09-13 13:35:21 -0300

    Adiciona pratica sobre computed property

--- ClassAndStruct.playground/Contents.swift
+++ ClassAndStruct.playground/Contents.swift
@@ -71,3 +71,41 @@ print("The framRate property of tenEighty is now \(tenEighty.frameRate)")
 if tenEighty === alsoTenEighty{
     print("The same")
 }
+
+
+
+
+// Computed property 
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Size {
+    var width  = 0.0, height = 0.0
+}
+
+struct Rect {
+    var origin  = Point()
+    var size = Size()
+    var center: Point {
+        get {
+            let centerX = origin.x + (size.width / 2)
+            let centerY = origin.y + (size.height / 2)
+            return Point(x: centerX, y: centerY)
+        }
+        set(newCenter){
+            origin.x = newCenter.x - (size.width / 2)
+            origin.y = newCenter.y - (size.height / 2)
+        }
+    }
+}
+
+var square = Rect(origin: Point(x: 0.0, y: 0.0),
+                  size: Size(width: 10.0, height: 10.0))
+
+let initialSquareCenter = square.center
+
+square.center = Point(x: 15.0, y: 15.0)
+
+print("square.origin is now at (\(square.origin.x), \(square.origin.y))")
