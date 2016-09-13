8a55331d0b3cc3c3c03607e81de82a96cc55ac10
Andre Dias <andre.dias@concrete.com.br>
2016-09-13 16:02:14 -0300

    Adiciona exercicio de Type property

--- ClassAndStruct.playground/Contents.swift
+++ ClassAndStruct.playground/Contents.swift
@@ -157,3 +157,35 @@ stepCounter.totalSteps = 800
 
 
 
+//Type property
+
+struct AudioChannel {
+    static let thresholdLevel = 10
+    static var maxInputLevelForAllChannels = 0
+    var currentLevel: Int = 0 {
+        didSet {
+            if currentLevel > AudioChannel.thresholdLevel {
+                currentLevel = AudioChannel.thresholdLevel
+            }
+            
+            if currentLevel > AudioChannel.maxInputLevelForAllChannels {
+                AudioChannel.maxInputLevelForAllChannels = currentLevel
+            }
+        }
+    }
+}
+
+var leftChannel = AudioChannel()
+var rightChannel = AudioChannel()
+
+leftChannel.currentLevel = 7
+print(leftChannel.currentLevel)
+
+print(AudioChannel.maxInputLevelForAllChannels)
+
+rightChannel.currentLevel = 11
+
+print(rightChannel.currentLevel)
+
+print(AudioChannel.maxInputLevelForAllChannels)
+
