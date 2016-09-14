9cdecbe8bda416d2b61145d9dde2ee4cc71ff05d
Andre Dias <andre.dias@concrete.com.br>
2016-09-14 17:06:48 -0300

    Adiciona arquivo mayor

---
+++ MonsterTown/MonsterTown/Mayor.swift
@@ -0,0 +1,13 @@
+//
+//  Mayor.swift
+//  MonsterTown
+//
+//  Created by Andre Dias on 9/14/16.
+//  Copyright © 2016 Andre Dias. All rights reserved.
+//
+
+struct Mayor {
+    static var pronunciation: String {
+        return "I'm deeply saddened to hear about this latest tragedy. I promise that my office is looking into the nature of this rash of violence."
+    }
+}
